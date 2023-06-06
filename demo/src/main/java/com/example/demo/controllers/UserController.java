d87a5326931f584f230ff514037916e35b406bf1
Pedro <pedrojuniorifpb@gmail.com>
2023-06-05 21:37:00 -0300

    Adicionando as rotas para o CRUD

---
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controllers;
+
+import com.example.demo.exceptions.UserNotFoundException;
+
+import com.example.demo.models.UserModel;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/user")
+public class UserController {
+    final UserRepository repository;
+
+    public UserController(UserRepository userRepository) {
+        this.repository = userRepository;
+    }
+
+    @GetMapping
+    public List<UserModel> getAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public UserModel getOne(@PathVariable UUID id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel newUser) {
+        return repository.save(newUser);
+    }
+
+    @PutMapping("/{id}")
+    public UserModel updateOne(@RequestBody UserModel newUser, @PathVariable UUID id) {
+    return repository.findById(id)
+      .map(user -> {
+        user.setNome(newUser.getNome());
+        user.setIdade(newUser.getIdade());
+        user.setSexo(newUser.getSexo());
+        user.setProfissao(newUser.getProfissao());
+        return repository.save(user);
+      })
+      .orElseGet(() -> {
+        newUser.setId(id);
+        return repository.save(newUser);
+      });
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOne(@PathVariable UUID id) {
+        repository.deleteById(id);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/models/UserModel.java
@@ -0,0 +1,64 @@
+package com.example.demo.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class UserModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, length = 200, name = "name")
+    private String nome;
+    @Column(nullable = false, name = "age")
+    private int idade;
+    @Column(nullable = false, name = "sex")
+    private char sexo;
+    @Column(nullable = false, name = "Employer")
+    private String profissao;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+}
